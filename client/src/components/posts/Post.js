2ed331d2b3043871a9ad6eada4074588929cd8a3
MrTj458 <mrtj458@gmail.com>
2019-08-31 19:00:45 -0600

    Posts list

---
+++ client/src/components/posts/Post.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Post = ({ post }) => {
+  return <h1>{post.title}</h1>
+}
+
+export default Post
--- client/src/components/posts/PostsList.js
+++ client/src/components/posts/PostsList.js
@@ -1,7 +1,51 @@
 import React from 'react'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
+import { withRouter } from 'react-router-dom'
 
-const PostsList = () => {
-  return <h1>Posts List</h1>
+import Post from './Post'
+
+const POSTS_QUERY = gql`
+  query POSTS_QUERY($topicId: Int!, $page: Int!, $limit: Int!) {
+    posts(topicId: $topicId, page: $page, limit: $limit) {
+      pageInfo {
+        page
+        pages
+        count
+      }
+      nodes {
+        id
+        title
+        user {
+          id
+          username
+        }
+        likes {
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
+const PostsList = ({ match }) => {
+  const page = Number(match.params.page)
+  const topicId = Number(match.params.id)
+
+  const { data, error, loading } = useQuery(POSTS_QUERY, {
+    variables: { page, topicId, limit: 5 },
+  })
+
+  if (loading) return <h1>Loading...</h1>
+  if (error) return <h1>Error: {error.message}</h1>
+
+  const posts = data.posts.nodes
+
+  if (posts.length === 0) return <h1>There are no posts</h1>
+
+  return posts.map(post => <Post key={post.id} post={post} />)
 }
 
-export default PostsList
+export default withRouter(PostsList)
